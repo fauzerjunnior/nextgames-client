b3590449fdd0012535b65d5ecfe9c333ad1ee1e0
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-01-11 14:49:15 -0300

    feat: add theme provider & create a theme custom render

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,13 @@
 import GlobalStyles from '@/styles/global';
+import theme from '@/styles/theme';
 import { PropsWithChildren } from 'react';
+import { ThemeProvider } from 'styled-components';
 
 export function Providers({ children }: PropsWithChildren) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       {children}
-    </>
+    </ThemeProvider>
   );
 }
