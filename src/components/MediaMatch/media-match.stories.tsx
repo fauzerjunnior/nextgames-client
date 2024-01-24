26d5f1c7c55d8fe99cee5764d6139ca6f773c4a6
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-01-24 12:24:06 -0300

    feat: add media match helper component

---
+++ src/components/MediaMatch/media-match.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta } from '@storybook/react';
+import MediaMatch from './media-match.component';
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta;
+
+export const Desktop = () => {
+  return (
+    <MediaMatch greaterThan="medium">
+      <h1>Only on Desktop</h1>
+    </MediaMatch>
+  );
+};
+
+export const Mobile = () => {
+  return (
+    <MediaMatch lessThan="medium">
+      <h1>Only on Mobile</h1>
+    </MediaMatch>
+  );
+};
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
+};
