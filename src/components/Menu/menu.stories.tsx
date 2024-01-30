b88533c1de209393f0de085bf54a0268da9f8256
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-01-30 16:37:16 -0300

    feat: add auth user menu versions

--- src/components/Menu/menu.stories.tsx
+++ src/components/Menu/menu.stories.tsx
@@ -1,12 +1,16 @@
 import { Meta, StoryObj } from '@storybook/react';
-import Menu from './menu.component';
+import Menu, { MenuProps } from './menu.component';
 
 export default {
   title: 'Menu',
   component: Menu
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj<MenuProps> = {
+  args: {
+    username: ''
+  }
+};
 
 Default.parameters = {
   layout: 'fullscreen',
--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -32,4 +32,22 @@ describe('Menu', () => {
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
     expect(fullMenuElement).toHaveStyle({ opacity: 0 });
   });
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />);
+
+    expect(screen.getByText(/Log in now/i)).toBeInTheDocument();
+    expect(screen.getByText(/Sign up/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/My account/i)).not.toBeInTheDocument();
+  });
+
+  it('should show closeds links when logged in', () => {
+    renderWithTheme(<Menu username="fauzerjunnior" />);
+
+    expect(screen.queryByText(/Log in now/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Sign up/i)).not.toBeInTheDocument();
+    expect(screen.getByText(/My account/i)).toBeInTheDocument();
+    expect(screen.getByText(/Wishlist/i)).toBeInTheDocument();
+  });
 });
