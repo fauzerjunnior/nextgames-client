1c6654be2dc27ffebc1a591adccbdda21d4601a0
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-02-14 00:40:40 -0300

    feat: add ribbon component

---
+++ src/components/Ribbon/ribbon.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryFn } from '@storybook/react';
+import Ribbon, { RibbonProps } from './ribbon.component';
+
+export default {
+  title: 'Ribbon',
+  component: Ribbon,
+  args: {
+    children: 'Best Seller',
+    color: 'primary',
+    size: 'normal'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    color: {
+      type: 'string'
+    },
+    size: {
+      type: 'string'
+    }
+  }
+} as Meta;
+
+export const Default: StoryFn<RibbonProps> = (args) => (
+  <div
+    style={{
+      width: '40rem',
+      height: '25rem',
+      position: 'relative',
+      backgroundColor: '#888'
+    }}
+  >
+    <Ribbon {...args} />
+  </div>
+);
