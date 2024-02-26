82361737c4b8e37b48e49e825d3926d069dca25e
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-02-26 11:43:41 -0300

    feat: add GameCard component initial structure

---
+++ src/components/GameCard/game-card.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryFn } from '@storybook/react';
+import GameCard, { GameCardProps } from './game-card.component';
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00'
+  }
+} as Meta;
+
+export const Default: StoryFn<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+);
