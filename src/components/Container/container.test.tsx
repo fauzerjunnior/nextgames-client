5bdaa6ae9873693c96c44b93630df9705cd7b7d6
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-02-29 10:57:47 -0300

    feat: add Container component

---
+++ src/components/Container/container.test.tsx
@@ -0,0 +1,20 @@
+import { Container } from './container.component';
+import { renderWithTheme } from '@/utils/tests/helpers';
+import theme from '@/styles/theme';
+
+describe('Container', () => {
+  it('should render the Container', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>Next Games</span>
+      </Container>
+    );
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
