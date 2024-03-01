518fc4a17746ec494c5e8433be6c4f282eb52348
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-03-01 17:49:33 -0300

    feat: add home structure and improve layout

--- src/templates/Home/home.component.tsx
+++ src/templates/Home/home.component.tsx
@@ -1,41 +1,93 @@
 'use client';
 
-import { Container, Menu, Footer, Heading } from '@/components';
+import {
+  Container,
+  Menu,
+  Footer,
+  Heading,
+  BannerSlider,
+  Highlight
+} from '@/components';
+import { BannerProps } from '@/components/Banner/banner.component';
+import { GameCardProps } from '@/components/GameCard/game-card.component';
+import { HighlightProps } from '@/components/Highlight/highlight.component';
+import GameCardSlider from '@/components/GameCardSlider/game-card-slider.component';
 
-const Home = () => {
+import * as S from './home.styles';
+
+export type HomeTemplateProps = {
+  banners: BannerProps[];
+  newGames: GameCardProps[];
+  mostPopularHighlight: HighlightProps;
+  mostPopularGames: GameCardProps[];
+  upcommingGames: GameCardProps[];
+  upcommingHighlight: HighlightProps;
+  upcommingMoreGames: GameCardProps[];
+  freeGames: GameCardProps[];
+  freeHighlight: HighlightProps;
+};
+const Home = ({
+  banners,
+  newGames,
+  upcommingGames,
+  freeHighlight,
+  mostPopularHighlight,
+  freeGames,
+  mostPopularGames,
+  upcommingHighlight,
+  upcommingMoreGames
+}: HomeTemplateProps) => {
   return (
     <section>
       <Container>
         <Menu />
+        <S.SectionBanner>
+          <BannerSlider items={banners} />
+        </S.SectionBanner>
       </Container>
 
-      <Container>
-        <Heading lineLeft lineColor="secondary" color="black">
-          News
-        </Heading>
-      </Container>
+      <S.SectionNews>
+        <Container>
+          <Heading lineLeft lineColor="secondary">
+            News
+          </Heading>
+
+          <GameCardSlider items={newGames} color="black" />
+        </Container>
+      </S.SectionNews>
 
       <Container>
-        <Heading lineLeft lineColor="secondary">
-          Most Popular
-        </Heading>
+        <S.SectionMostPopular>
+          <Heading lineLeft lineColor="secondary">
+            Most Popular
+          </Heading>
+          <Highlight {...mostPopularHighlight} />
+          <GameCardSlider items={mostPopularGames} />
+        </S.SectionMostPopular>
+
+        <S.SectionUpcoming>
+          <Heading lineLeft lineColor="secondary">
+            Upcomming
+          </Heading>
+          <GameCardSlider items={upcommingGames} />
+          <Highlight {...upcommingHighlight} />
+          <GameCardSlider items={upcommingMoreGames} />
+        </S.SectionUpcoming>
+
+        <S.SectionFreeGames>
+          <Heading lineLeft lineColor="secondary">
+            Free games
+          </Heading>
+          <Highlight {...freeHighlight} />
+          <GameCardSlider items={freeGames} />
+        </S.SectionFreeGames>
       </Container>
 
-      <Container>
-        <Heading lineLeft lineColor="secondary">
-          Upcomming
-        </Heading>
-      </Container>
-
-      <Container>
-        <Heading lineLeft lineColor="secondary">
-          Free Games
-        </Heading>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
+      <S.SectionFooter>
+        <Container>
+          <Footer />
+        </Container>
+      </S.SectionFooter>
     </section>
   );
 };
